409a7a94915387f75f3a827547eb00f28f452836
piko <51285796+darkzcliff@users.noreply.github.com>
2023-03-06 20:05:44 +0700

    Add files via upload

---
+++ modul1/latihan2/Makanan.java
@@ -0,0 +1,7 @@
+package private_basis_data.latihan2;
+
+public class Makanan extends Hidangan{
+    public String disantap(){
+        return this.getNamaHidangan() + " dimakan";
+    }
+}
---
+++ modul1/latihan2/Minuman.java
@@ -0,0 +1,7 @@
+package private_basis_data.latihan2;
+
+public class Minuman extends Hidangan{
+    public String disantap(){
+        return this.getNamaHidangan() + " diminum";
+    }
+}
